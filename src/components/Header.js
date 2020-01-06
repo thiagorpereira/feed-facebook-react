72c72c4608bfe597e27424c71f04259eaa41ce78
Thiago Pereira <thiagorpereira7@gmail.com>
2020-01-06 20:10:03 -0200

    Add header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header(){
+  return(
+    <header>
+      <img src="https://i.imgur.com/KDIDiSE.png" alt="Facebook"/>
+      <div class="side-right">
+        <span>Meu perfiiil</span>
+        <i className="material-icons">account_circle</i>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
